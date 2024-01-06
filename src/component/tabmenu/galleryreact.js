099c51988ba989aee44e11455df960ebb767beec
fatima678 <119948111+fatima678@users.noreply.github.com>
2024-01-06 23:46:09 +0500

    filter image gallery using react js

---
+++ src/component/tabmenu/galleryreact.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const galleryreact = () => {
+  return (
+    <div className='container'>
+        <h1>Order Your Favourite Dish</h1>
+        <hr></hr>
+    </div>
+    
+  )
+}
+
+export default galleryreact
